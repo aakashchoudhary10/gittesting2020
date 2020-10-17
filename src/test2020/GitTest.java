123d880720ff6d2923044ea7dce5dc4f44b316ff
Aakash Choudhary <aakashchoudhary@DLLA1660.qait.com>
2020-10-17 19:33:34 +0530

    first commit

---
+++ src/test2020/GitTest.java
@@ -0,0 +1,14 @@
+package test2020;
+
+public class GitTest {
+
+	public static void main(String[] args) {
+		int a = 1;
+		int b = 9;
+		
+	int c = a + b;
+	System.out.println("Value of C: " + c);
+
+	}
+
+}
